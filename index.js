3752a65eab2995520d3c3d04bc324ac748b53214
shayan-hamidi <sha.hamidi10@gmail.com>
2021-12-27 18:00:05 +0330

    add ProtoType

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-function Nav() {
+function Nav({src}) {
   return(
     <nav id="nav">
     <button onClick={()=> console.log("backClick")}><i className="fa fa-angle-left backBtn"></i></button>
-    <div id="prifileNameBox"><h1>{"instagram"}</h1><img src="./images/blueTick.png" alt="blueTick" width="40" height="25" /></div>
+    <div id="profileNameBox"><h1>{"instagram"}</h1><img src= {src} alt="blueTick" width="40" height="25" /></div>
     <button onClick={()=> console.log("menuClick")}><svg aria-label="Options" className="_8-yf5 " color="#262626" fill="#262626" height="32" role="img" viewBox="0 0 24 24" width="32"><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg></button>
     </nav>
   );
 }
-function ProfileBox() {
+Nav.prototype={
+  src:PropTypes.string,
+}
+function ProfileBox({src}) {
   return(
   <div className="profileBox">
   <div className="profileDetails">
-  <div id="profileImgBox"><img src="./images/profile-instagram.jpg" alt="instagram-profile" width="150" height="150" /></div> 
+  <div id="profileImgBox"><img src= {src} alt="instagram-profile" width="150" height="150" /></div> 
    <div className="countBox">
        <div className="profileStatistics"><span>{"6,368"}</span><span>{"Posts"}</span></div>
        <div className="profileStatistics"><span>{"350 M"}</span><span>{"Followers"}</span></div>
@@ -21,14 +24,20 @@ function ProfileBox() {
    </div>
    );
 }
-function BioBox() {
+ProfileBox.prototype={
+  src:PropTypes.string,
+}
+function BioBox({href}) {
   return(
     <section className="bioBox">
     <h3>{"Instagram"}</h3>
     <p>Bringing you closer to the people and things you <br />love. ❤️</p>
-    <a href="https://www.facebook.com/community_help/?page_source=fb_marketing_covid_mar_campaign">{"www.facebook.com/covidsupport"}</a>
+    <a href={href}>{"www.facebook.com/covidsupport"}</a>
 </section>
   );
+}
+BioBox.prototype={
+  href:PropTypes.string,
 }
 function ActionBox() {
   return(
@@ -42,9 +51,9 @@ function ActionBox() {
 function App() {
   return(
     <div id="container">
-      <Nav />
-      <ProfileBox />
-      <BioBox />
+      <Nav src={"./images/blueTick.png"} />
+      <ProfileBox src={"./images/profile-instagram.jpg"} />
+      <BioBox href={"https://www.facebook.com/community_help/?page_source=fb_marketing_covid_mar_campaign"} />
       <ActionBox />
     </div>
   );
